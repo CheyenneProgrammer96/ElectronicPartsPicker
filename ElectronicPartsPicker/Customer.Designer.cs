e87cf00b6913802083b18bc475d036a567a1031e
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-01 14:38:22 -0800

    Created basic outline for order form

---
+++ ElectronicPartsPicker/Customer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ElectronicPartsPicker
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 819);
+            this.Name = "Customer";
+            this.Text = "Customer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- ElectronicPartsPicker/Form1.Designer.cs
+++ ElectronicPartsPicker/Form1.Designer.cs
@@ -28,19 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnDeleteProduct = new System.Windows.Forms.Button();
+            this.cboxProductMenu = new System.Windows.Forms.ComboBox();
+            this.txtOrderTotal = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblOrderTotal = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(652, 274);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(198, 124);
+            this.btnAddProduct.TabIndex = 0;
+            this.btnAddProduct.Text = "Add Product";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteProduct
+            // 
+            this.btnDeleteProduct.Location = new System.Drawing.Point(652, 516);
+            this.btnDeleteProduct.Name = "btnDeleteProduct";
+            this.btnDeleteProduct.Size = new System.Drawing.Size(198, 113);
+            this.btnDeleteProduct.TabIndex = 1;
+            this.btnDeleteProduct.Text = "Delete Product";
+            this.btnDeleteProduct.UseVisualStyleBackColor = true;
+            // 
+            // cboxProductMenu
+            // 
+            this.cboxProductMenu.FormattingEnabled = true;
+            this.cboxProductMenu.Location = new System.Drawing.Point(192, 216);
+            this.cboxProductMenu.Name = "cboxProductMenu";
+            this.cboxProductMenu.Size = new System.Drawing.Size(242, 40);
+            this.cboxProductMenu.TabIndex = 2;
+            // 
+            // txtOrderTotal
+            // 
+            this.txtOrderTotal.Location = new System.Drawing.Point(214, 631);
+            this.txtOrderTotal.Name = "txtOrderTotal";
+            this.txtOrderTotal.Size = new System.Drawing.Size(200, 39);
+            this.txtOrderTotal.TabIndex = 3;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Location = new System.Drawing.Point(214, 562);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(200, 39);
+            this.txtTax.TabIndex = 4;
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(214, 495);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(200, 39);
+            this.txtSubtotal.TabIndex = 5;
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(43, 495);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(108, 32);
+            this.lblSubtotal.TabIndex = 6;
+            this.lblSubtotal.Text = "Subtotal:";
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(43, 562);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(59, 32);
+            this.lblTax.TabIndex = 7;
+            this.lblTax.Text = "Tax: ";
+            // 
+            // lblOrderTotal
+            // 
+            this.lblOrderTotal.AutoSize = true;
+            this.lblOrderTotal.Location = new System.Drawing.Point(43, 639);
+            this.lblOrderTotal.Name = "lblOrderTotal";
+            this.lblOrderTotal.Size = new System.Drawing.Size(138, 32);
+            this.lblOrderTotal.TabIndex = 8;
+            this.lblOrderTotal.Text = "Order Total:";
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(53, 219);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(101, 32);
+            this.lblProduct.TabIndex = 9;
+            this.lblProduct.Text = "Product:";
             // 
             // FrmOrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1095, 854);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.lblOrderTotal);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.txtOrderTotal);
+            this.Controls.Add(this.cboxProductMenu);
+            this.Controls.Add(this.btnDeleteProduct);
+            this.Controls.Add(this.btnAddProduct);
             this.Name = "FrmOrderForm";
             this.Text = "Order Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button btnAddProduct;
+        private Button btnDeleteProduct;
+        private ComboBox cboxProductMenu;
+        private TextBox txtOrderTotal;
+        private TextBox txtTax;
+        private TextBox txtSubtotal;
+        private Label lblSubtotal;
+        private Label lblTax;
+        private Label lblOrderTotal;
+        private Label lblProduct;
     }
 }
