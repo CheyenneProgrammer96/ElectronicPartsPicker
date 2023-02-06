dbb0dd63ab44860489bab6e9541d8b3efcbe365d
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-06 07:41:12 -0800

    Added txt boxes and labels to customer form

--- ElectronicPartsPicker/Customer.Designer.cs
+++ ElectronicPartsPicker/Customer.Designer.cs
@@ -28,19 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtOrderNumber = new System.Windows.Forms.TextBox();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblOrderNumber = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblEmailAddress = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(304, 82);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 39);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(304, 263);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 39);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtOrderNumber
+            // 
+            this.txtOrderNumber.Location = new System.Drawing.Point(304, 167);
+            this.txtOrderNumber.Name = "txtOrderNumber";
+            this.txtOrderNumber.Size = new System.Drawing.Size(200, 39);
+            this.txtOrderNumber.TabIndex = 2;
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(304, 353);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(200, 39);
+            this.txtEmailAddress.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(96, 82);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(188, 32);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Customer Name";
+            // 
+            // lblOrderNumber
+            // 
+            this.lblOrderNumber.AutoSize = true;
+            this.lblOrderNumber.Location = new System.Drawing.Point(96, 167);
+            this.lblOrderNumber.Name = "lblOrderNumber";
+            this.lblOrderNumber.Size = new System.Drawing.Size(170, 32);
+            this.lblOrderNumber.TabIndex = 5;
+            this.lblOrderNumber.Text = "Order Number";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(115, 263);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(98, 32);
+            this.lblAddress.TabIndex = 6;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblEmailAddress
+            // 
+            this.lblEmailAddress.AutoSize = true;
+            this.lblEmailAddress.Location = new System.Drawing.Point(96, 360);
+            this.lblEmailAddress.Name = "lblEmailAddress";
+            this.lblEmailAddress.Size = new System.Drawing.Size(162, 32);
+            this.lblEmailAddress.TabIndex = 7;
+            this.lblEmailAddress.Text = "Email Address";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(96, 463);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(177, 32);
+            this.lblPhoneNumber.TabIndex = 8;
+            this.lblPhoneNumber.Text = "Phone Number";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(304, 456);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(200, 39);
+            this.txtPhoneNumber.TabIndex = 9;
             // 
             // Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 819);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblEmailAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblOrderNumber);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.txtOrderNumber);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
             this.Name = "Customer";
-            this.Text = "Customer";
+            this.Text = "Customer Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox txtName;
+        private TextBox txtAddress;
+        private TextBox txtOrderNumber;
+        private TextBox txtEmailAddress;
+        private Label lblName;
+        private Label lblOrderNumber;
+        private Label lblAddress;
+        private Label lblEmailAddress;
+        private Label lblPhoneNumber;
+        private TextBox txtPhoneNumber;
     }
 }
