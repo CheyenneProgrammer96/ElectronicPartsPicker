3e121f14d4d09d6ea1a017210f692c0f067af46b
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-08 14:26:26 -0800

    Created Order database and order class

---
+++ ElectronicPartsPicker/Data/OrderContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicPartsPicker
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ ElectronicPartsPicker/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicPartsPicker
+{
+    /// <summary>
+    /// Represents a single order to be placed
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// The unique order number 
+        /// </summary>
+        [Key]
+        public int OrderNumber { get; set; }
+
+        /// <summary>
+        /// The name of the customer
+        /// </summary>
+        public char CustomerName { get; set; }
+        
+        /// <summary>
+        /// The name of the part being ordered
+        /// </summary>
+        public char PartName { get; set; }
+
+        /// <summary>
+        /// The sales price of the parts
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The total price amount of the parts being ordered
+        /// </summary>
+        public decimal PriceTotal { get; set;}
+    }
+}
