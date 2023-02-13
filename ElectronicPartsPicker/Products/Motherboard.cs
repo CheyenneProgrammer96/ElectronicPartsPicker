e3637817c0b030a668225c895e72697a0ca9cb75
Armiger21 <Porfirdruid1@gmail.com>
2023-02-13 12:34:37 -0800

    Added Motherboard class

---
+++ ElectronicPartsPicker/Products/Motherboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicPartsPicker.Products
+{
+    internal class Motherboard
+    {
+        public int MotherboardId { get; set; }
+        public string MotherboardName { get; set; }
+        public double MotherboardPrice { get; set; }
+    }
+}
