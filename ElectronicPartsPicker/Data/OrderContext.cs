dda6dd6c8635192c453c9a35d3facbdcb35e6055
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-13 16:47:55 -0800

    Added DbSet for products folder

--- ElectronicPartsPicker/Data/OrderContext.cs
+++ ElectronicPartsPicker/Data/OrderContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ElectronicPartsPicker.Products;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,17 @@ namespace ElectronicPartsPicker
         }
 
         public DbSet<Order> Orders { get; set; }
+
+        public DbSet<gpu> GPUs { get; set; }
+
+        public DbSet<Memory> Memorys { get; set; }
+
+        public DbSet<Motherboard> Motherboards { get; set;}
+
+        public DbSet<Nic> Nic { get; set; }
+
+        public DbSet<Processor> Processor { get; set; }
+
+
     }
 }
