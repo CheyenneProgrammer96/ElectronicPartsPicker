e9633c1b0c4555b2692ba49c8819cff1dd957940
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-13 16:48:48 -0800

    Added new migration into Db for products folder

--- ElectronicPartsPicker/Migrations/20230214004651_Products.Designer.cs
+++ ElectronicPartsPicker/Migrations/20230214004651_Products.Designer.cs
@@ -3,6 +3,7 @@ using ElectronicPartsPicker;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElectronicPartsPicker.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20230214004651_Products")]
+    partial class Products
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ElectronicPartsPicker/Migrations/20230214004651_Products.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectronicPartsPicker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Products : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GPUs",
+                columns: table => new
+                {
+                    gpuId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    gpuValue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GPUs", x => x.gpuId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memorys",
+                columns: table => new
+                {
+                    MemoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    memoryValue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memorys", x => x.MemoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Motherboards",
+                columns: table => new
+                {
+                    MotherboardId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MotherboardName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MotherboardPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motherboards", x => x.MotherboardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nic",
+                columns: table => new
+                {
+                    NicId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NicName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NicValue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nic", x => x.NicId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Processor",
+                columns: table => new
+                {
+                    ProcessorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProcessorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProcessorValue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processor", x => x.ProcessorId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GPUs");
+
+            migrationBuilder.DropTable(
+                name: "Memorys");
+
+            migrationBuilder.DropTable(
+                name: "Motherboards");
+
+            migrationBuilder.DropTable(
+                name: "Nic");
+
+            migrationBuilder.DropTable(
+                name: "Processor");
+        }
+    }
+}
