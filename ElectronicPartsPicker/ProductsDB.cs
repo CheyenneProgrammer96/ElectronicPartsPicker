8513627013ae914f472eb2ceed7f915192d34e51
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-02-22 13:09:07 -0800

    Created Database for products class

---
+++ ElectronicPartsPicker/ProductsDB.cs
@@ -0,0 +1,19 @@
+﻿using ElectronicPartsPicker.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicPartsPicker
+{
+    public class ProductsDB : OrderContext
+    {
+        private readonly OrderContext _context;
+
+        public ProductsDB(OrderContext context)
+        {
+            _context = context;
+        }
+    }
+}
