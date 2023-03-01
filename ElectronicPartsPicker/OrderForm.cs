67695201200a9e49867c0ce6f57ddc90c7c42c21
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-03-01 14:17:39 -0800

    Added products listed in the form load event to pull the products from the database

--- ElectronicPartsPicker/OrderForm.cs
+++ ElectronicPartsPicker/OrderForm.cs
@@ -1,3 +1,5 @@
+using ElectronicPartsPicker.Products;
+
 namespace ElectronicPartsPicker
 {
     public partial class FrmOrderForm : Form
@@ -5,6 +7,41 @@ namespace ElectronicPartsPicker
         public FrmOrderForm()
         {
             InitializeComponent();
+        }
+
+        public void cboxProductMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FrmOrderForm_Load(object sender, EventArgs e)
+        {
+            OrderContext context = new OrderContext();
+
+            List<Processor> processors = (from processor in context.Processor
+                                          select processor).ToList();
+
+            List<gpu> gpus = (from gpu in context.GPUs
+                              select gpu).ToList();
+
+            List<Motherboard> motherboards = (from Motherboard in context.Motherboards
+                                              select Motherboard).ToList();
+
+            List<Nic> nics = (from Nic in context.Nic
+                              select Nic).ToList();
+
+            List<Memory> memory = (from Memory in context.Memorys
+                                   select Memory).ToList();
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            comboBoxProduct.Items.Add(comboBoxProduct);
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            comboBoxProduct.Items.Remove(comboBoxProduct.SelectedItem);
         }
     }
 }
